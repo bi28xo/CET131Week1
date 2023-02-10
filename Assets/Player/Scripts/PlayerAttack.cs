6698212736d32c0bf23ed47a36e2e46cefb1537c
bi28xo <bi28xo@student.sunderland.ac.uk>
2023-02-10 19:29:27 +0000

    Added Player and Enemy Shooting/Raycast code

--- Assets/Player/Scripts/PlayerAttack.cs
+++ Assets/Player/Scripts/PlayerAttack.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    Transform cameraTransform;
+    float range = 100f;
+
+    [SerializeField]
+    float rawDamage = 10f;
 
     // Update is called once per frame
     void Update()
     {
-        
+        FireWeapon(); 
+    }
+
+    void FireWeapon()
+    {
+        if (Input.GetButtonDown("Fire 1"))
+        {
+            cameraTransform = Camera.main.transform;
+            Ray ray = new Ray(cameraTransform.position, cameraTransform.forward);
+            RaycastHit raycastHit;
+
+            if (Physics.Raycast(ray, out raycastHit, range))
+            {
+                if (raycastHit.transform != null)
+                {
+                    raycastHit.collider.SendMessageUpwards("Hit", rawDamage, SendMessageOptions.DontRequireReceiver);
+                }
+            }
+            else
+            {
+                Debug.Log("NO RAYCAST");
+            }
+        }
     }
 }
